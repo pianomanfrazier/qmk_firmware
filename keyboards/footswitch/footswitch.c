20b8d03c03b47e9b6e6a7f97bc876d00f7856e49
Ryan Frazier <{ID}+{username}@users.noreply.github.com>
2025-03-27 21:40:59 -0600

    Add some comments and make default more simple

--- keyboards/footswitch/footswitch.c
+++ keyboards/footswitch/footswitch.c
@@ -2,14 +2,14 @@
 
 // MUST HAVE BOOTMAGIC ENABLED
 void early_init_user(void) {
-    setPinInputHigh(F4);  // First footswitch pin
-    setPinInputHigh(F5);  // Second footswitch pin
+    setPinInputHigh(F4);  // First footswitch pin, A3 -> Tip + Sleeve
+    setPinInputHigh(F5);  // Second footswitch pin, A2 -> Ring + Sleeve
 
     // Wait a moment to stabilize pin state
     wait_ms(10);
 
+    // Both buttons held = jump to bootloader
     if (!readPin(F4) && !readPin(F5)) {
-        // Both buttons held = jump to bootloader
         bootloader_jump();
     }
 }
--- keyboards/footswitch/keymaps/default/keymap.c
+++ keyboards/footswitch/keymaps/default/keymap.c
@@ -3,7 +3,9 @@
 // Keymap definition
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        LCTL(LALT(KC_J)),
-        LCTL(LALT(KC_T))
+        // LCTL(LALT(KC_J)),
+        // LCTL(LALT(KC_T))
+        KC_A,
+        KC_B
     )
 };
